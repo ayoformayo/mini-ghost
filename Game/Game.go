c12a7e504842a07dd37b55b6c2d62fbcb32f92bf
Jimmi Carney <jamescarney10@gmail.com>
2017-07-15 22:08:33 -0400

    populating players

--- Game/Game.go
+++ Game/Game.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 
 	"github.com/ayoformayo/mini-ghost/Player"
 	"github.com/ayoformayo/mini-ghost/Round"
@@ -17,11 +19,33 @@ type Game struct {
 	// letter
 }
 
+func getPlayerCount(reader *bufio.Reader) int {
+	playerNumber, _ := reader.ReadString('\n')
+	strippedNumber := strings.TrimSpace(playerNumber)
+	number, err := strconv.Atoi(strippedNumber)
+	if err != nil {
+		fmt.Println("Whoops! Not a valid number. Try again")
+		getPlayerCount(reader)
+	}
+	return number
+}
+
+func (game *Game) populatePlayers(count int) {
+	i := 0
+	for i < count {
+		game.Players = append(game.Players, Player.Player{})
+		i++
+	}
+}
+
 // StartGame does something
-func (p *Game) StartGame() {
-	fmt.Println("Hi, my name is", p.Name)
+func (game *Game) StartGame() {
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter text: ")
-	text, _ := reader.ReadString('\n')
-	fmt.Println(text)
+	fmt.Print("Welcome to Ghost! Please select number of players - max 5\n")
+	playerCount := getPlayerCount(reader)
+	game.populatePlayers(playerCount)
+	fmt.Println(game.Players)
+	fmt.Print("What will the first letter be?\n")
+	firstLetter, _ := reader.ReadString('\n')
+	fmt.Println(firstLetter)
 }
