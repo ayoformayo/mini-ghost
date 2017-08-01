3db9e2c09e2ee7f63d104170fb34a8e07227c2bc
Jimmi Carney <jamescarney10@gmail.com>
2017-08-01 19:12:32 -0400

    adding tree test

--- Tree/Tree_test.go
+++ Tree/Tree_test.go
@@ -8,19 +8,51 @@ import (
 
 const targetTestVersion = 1
 
-var fakeDictionary = []string{
+var trueDictionary = []string{
 	"AAHED",
 	"AAHING",
 	"ZIBELINE",
 	"ZIBELLINE",
 }
 
-var wordTree = tree.BuildWordTree(fakeDictionary)
+var fakeDictionary = []string{
+	"AAHD",
+	"AAZING",
+	"PPIBELINE",
+	"FOOBAR",
+}
+
+var wordTree = tree.BuildWordTree(trueDictionary)
 
 func TestFragmentIsWord(t *testing.T) {
-	for _, word := range fakeDictionary {
+	for _, word := range trueDictionary {
 		if !wordTree.FragmentIsWord(word) {
 			t.Errorf("Tree didn't recognize %s as a word", word)
+		}
+	}
+}
+
+func TestFragmentIsNotWord(t *testing.T) {
+	for _, fake := range fakeDictionary {
+		if wordTree.FragmentIsWord(fake) {
+			t.Errorf("Tree recognized %s as a playable word", fake)
+		}
+	}
+}
+
+func TestFragmentIsEligible(t *testing.T) {
+	for _, word := range trueDictionary {
+		truncated := word[:3]
+		if !wordTree.IsEligible(truncated) {
+			t.Errorf("Tree didn't recognize %s as an eligible phrase", truncated)
+		}
+	}
+}
+
+func TestFragmentIsNotEligible(t *testing.T) {
+	for _, fake := range fakeDictionary {
+		if wordTree.IsEligible(fake) {
+			t.Errorf("Tree recognized %s as an legitimate phrase", fake)
 		}
 	}
 }
